c1cc604d05abe6662a7e56f3ac805ba20e8b3aa1
Roger Wang <rogerwang2520@gmail.com>
2021-11-09 20:58:45 -0600

    Added default RocketData struct function

--- src/csvreader.rs
+++ src/csvreader.rs
@@ -63,6 +63,28 @@ pub struct RocketData {
 
 impl Default for RocketData {
     fn default() -> Self {
-        return Default::default();
+        return RocketData {
+            timestamp: 0,
+            ax: 0.0,
+            ay: 0.0,
+            az: 0.0,
+            gx: 0.0,
+            gy: 0.0,
+            gz: 0.0,
+            mx: 0.0,
+            my: 0.0,
+            mz: 0.0,
+            latitude: 0.0,
+            longitude: 0.0,
+            altitude: 0.0,
+            satellite_count: 0,
+            position_lock: 0,
+            temperature: 0.0,
+            pressure: 0.0,
+            barometer_altitude: 0.0,
+            rocket_state: 0,
+            l1_extension: 0.0,
+            l2_extension: 0.0,
+        };
     }
 }
--- src/mvpmatrix.rs
+++ src/mvpmatrix.rs
@@ -3,13 +3,14 @@ use glm::Vector3;
 use glm::Matrix4;
 use map_3d;
 use num;
+use cgmath::One;
 
 use crate::csvreader::RocketData;
 use crate::csvreader::ROCKET_DATA;
 
 static rocket_data_row: RocketData = RocketData::default();
 
-pub fn get_look_at() -> Matrix4<f64> {
+pub fn get_look_at() -> Matrix4<f64> {  // TODO: Change to reflect ecef coordinates
   let eye = Vector3::new(0.0, 0.0, 0.0);
   let center = Vector3::new(0.0, 1.0, 0.0);
   let up = Vector3::new(0.0, 1.0, 0.0);
@@ -17,14 +18,14 @@ pub fn get_look_at() -> Matrix4<f64> {
 }
 
 pub fn get_rotate() -> Matrix4<f64> {
-  glm::mat4 m4( 1.0f );
-  let angle;
-  let axis;
-  return glm::ext::rotate();
+  let angle = 0.0;
+  let axis = Vector3::new(0.0, 1.0, 0.0);
+  return glm::ext::rotate(&num::one(), angle, axis);
 }
 
 pub fn get_rocket_translate() -> Matrix4<f64> {
-  let ecef = map_3d::geodetic2ecef(rocket_data_row.latitude, rocket_data_row.longitude, rocket_data_row.altitude).to_vec();
+  let (ecef_x, ecef_y, ecef_z) = map_3d::geodetic2ecef(rocket_data_row.latitude, rocket_data_row.longitude, rocket_data_row.altitude);
+  let ecef = Vector3::new(ecef_x, ecef_y, ecef_z);
   return glm::ext::translate(&num::one(), ecef);
 }
 
@@ -32,8 +33,13 @@ pub fn get_earth_translate() -> Matrix4<f64> {
   todo!();
 }
 
+pub fn get_scale() -> Matrix4<f64> {
+  return glm::ext::scale(&num::one(), Vector3::one());
+}
+
 pub fn get_mvp(csv_row_num: usize) -> Matrix4<f64> {
   rocket_data_row = ROCKET_DATA[csv_row_num];
   let rocket_translate = get_rocket_translate();
+  // [Rocket translate] * [Rotate] * [Identity]
   todo!();
 }
